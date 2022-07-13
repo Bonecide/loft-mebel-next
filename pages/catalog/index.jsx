703ed525abf19977d71bd15a62b48e0688f50cd3
Kasymov Marsel <marselkasymov23@gmail.com>
2022-07-13 22:06:14 +0600

    added catalog page

---
+++ pages/catalog/index.jsx
@@ -0,0 +1,12 @@
+
+import Layout from './../../Components/Layout/Layout';
+export default function Catalog() {
+
+    return(
+        <Layout>
+            <div>
+                
+            </div>
+        </Layout>
+    )
+}
